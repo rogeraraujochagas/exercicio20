b7cc902752bd3886eb8d76c0ccebcf999df5a351
rogerio <rogeri_22@hotmail.com>
2019-05-08 20:07:48 -0300

    Ex. 19 - Revisão de JPA

---
+++ src/main/java/br/com/etechoracio/exercicio20/Exercicio20Application.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.exercicio20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercicio20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercicio20Application.class, args);
+	}
+
+}
